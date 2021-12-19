3e5ebe30db4d77b3059e936705344b604ffaa87f
IsmaDavidAA <idaasannmvgd@gmail.com>
2021-12-19 12:50:07 -0400

    agregar request get-post-put-delete

--- controllers/diagramController.js
+++ controllers/diagramController.js
@@ -4,15 +4,46 @@ const { body, validationResult } = require("express-validator");
 
 var async = require("async");
 
-exports.index = function (req, res) {
-  async.parallel({}, function (err, results) {
-    res.status(200).json({
-      message: "Ok",
+exports.diagram_get_all = [
+  (req, res, next) => {
+    Diagram.find({}, function (err, diagrams) {
+      if (err) {
+        res.status(500).json({
+          error: err,
+        });
+      }
+      res.status(200).json({
+        message: "Diagrams retrieved successfully",
+        data: diagrams,
+      });
     });
-  });
-};
+  },
+];
+
+exports.diagram_get_by_id = [
+  (req, res, next) => {
+    Diagram.findById(req.params.diagramId, function (err, diagram) {
+      if (err) {
+        res.status(500).json({
+          error: err,
+        });
+      }
+      res.status(200).json({
+        message: "Diagram retrieved successfully",
+        data: diagram,
+      });
+    });
+  },
+];
 
 exports.diagram_post = [
+  //validate the request
+  body("diagramName")
+    .isLength({ min: 1 })
+    .trim()
+    .withMessage("Diagram name must be specified."),
+  body("classes").notEmpty().withMessage("Classes must be specified."),
+  //process the request
   (req, res, next) => {
     // Extract the validation errors from a request.
     const errors = validationResult(req);
@@ -23,7 +54,6 @@ exports.diagram_post = [
       creationDate: Date.now(),
       classes: req.body.classes,
     });
-    // diagram.id = req.params.id;
     if (!errors.isEmpty()) {
       async.parallel(function (err, results) {
         if (err) {
@@ -54,5 +84,53 @@ exports.diagram_post = [
         });
       });
     }
+  },
+];
+
+exports.diagram_update_by_id = [
+  //validate the request
+  body("diagramName")
+    .isLength({ min: 1 })
+    .trim()
+    .withMessage("Diagram name must be specified."),
+  body("classes").notEmpty().withMessage("Classes must be specified."),
+  //process the request
+  (req, res, next) => {
+    const errors = validationResult(req);
+    Diagram.findById(req.params.diagramId, function (err, diagram) {
+      if (err) {
+        res.status(500).json({
+          error: err,
+        });
+      }
+      diagram.diagramName = req.body.diagramName;
+      diagram.creationDate = Date.now();
+      diagram.classes = req.body.classes;
+      diagram.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        res.status(200).json({
+          message: "Diagram updated successfully",
+          data: diagram,
+        });
+      });
+    });
+  },
+];
+
+exports.diagram_delete_by_id = [
+  (req, res, next) => {
+    Diagram.findByIdAndRemove(req.params.diagramId, function (err, diagram) {
+      if (err) {
+        res.status(500).json({
+          error: err,
+        });
+      }
+      res.status(200).json({
+        message: "Diagram deleted successfully",
+        data: diagram,
+      });
+    });
   },
 ];
--- routes/api.js
+++ routes/api.js
@@ -4,4 +4,12 @@ var diagram_controller = require("../controllers/diagramController");
 // POST request for creating api.
 router.post("/diagram/", diagram_controller.diagram_post);
 
+router.get("/diagram/", diagram_controller.diagram_get_all);
+
+router.get("/diagram/:diagramId", diagram_controller.diagram_get_by_id);
+
+router.put("/diagram/:diagramId", diagram_controller.diagram_update_by_id);
+
+router.delete("/diagram/:diagramId", diagram_controller.diagram_delete_by_id);
+
 module.exports = router;
