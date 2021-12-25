a7fa00d389d80c0bad5633724a284f66090333c0
IsmaDavidAA <idaasannmvgd@gmail.com>
2021-12-25 11:34:37 -0400

    agregar enrollmentController y userModel

--- models/taskModel.js
+++ models/taskModel.js
@@ -7,13 +7,12 @@ var TaskSchema = new Schema({
   description: { type: String, required: true, maxLength: 500 },
   courseId: { type: String, required: true },
   creationDate: { type: Date },
-  classes: Object,
 });
 
 // Virtual for author's URL
 TaskSchema.virtual("url").get(function () {
-  return "/api/diagrams/" + this._id;
+  return "/api/tasks/" + this._id;
 });
 
 //Export model
-module.exports = mongoose.model("Diagram", TaskSchema);
+module.exports = mongoose.model("Task", TaskSchema);
